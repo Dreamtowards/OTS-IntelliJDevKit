cfab9b9d6384b61854b3102214de97963bc7380e
GitHub Action <action@github.com>
2021-02-18 04:24:44 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dreamtowards/otsintellijdevkit/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dreamtowards.otsintellijdevkit.services
+
+import com.github.dreamtowards.otsintellijdevkit.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
